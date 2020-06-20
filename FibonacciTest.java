fb8d0f2689093273d74f435c98dc05dc23000d47
VEZ-NAN <1987712358@qq.com>
2020-06-20 19:54:07 +0800

    commit README.md

---
+++ FibonacciTest.java
@@ -0,0 +1,30 @@
+public class FibonacciTest
+{
+    public static void main(String[] args)
+    {
+        int result=0;
+        for(int i=1;;++i)
+        {
+            result=Fibonacci.of(i);
+            if(result>200)break;
+            System.out.println(result);
+        }
+    }
+}
+
+class Fibonacci
+{
+    static int of(int n)
+    {
+        int a=1;
+        int b=1;
+        int temp;
+        for(int i=3;i<=n;++i)
+        {
+            temp=a;
+            a=b;
+            b+=temp;
+        }
+        return b;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+FibonacciTest.java说明：
+FibonacciTest类：测试类，含main方法，运行即可打印数值位于1与200之间的Fibonacci数，每个数占一行。
+Fibonacci类：含有一个静态方法of，该方法以一个名为n代表Fibonacci数列项数的int为参数，返回一个代表Fibonacci数列第n项的值的int。
+
+姓名：吴俊东。学号：19030500331。GitHub地址：
